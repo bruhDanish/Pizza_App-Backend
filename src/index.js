d083e015d7a1f3744b14ba373d432bb1948037f9
unknown <syeddh01@gmail.com>
2024-12-27 21:48:25 +0530

    express starter app completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express= require('express');
+const bodyParser = require('body-parser');
 
 const serverConfig = require('./config/server.config.js');
 const connectDB = require('./config/db.config.js');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.text()); //works same as bodyParser.text()
+
+app.post('/ping', (req, res) => {
+    console.log(req.body);
+    return res.json({ message: 'pong' });
+})
 
 app.listen(serverConfig.PORT, async () => {
     await connectDB();
